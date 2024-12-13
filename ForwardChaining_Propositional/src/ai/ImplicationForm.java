4f151c7c810be4da594b50353c17956bf6171a17
BabisDros <150071850+BabisDros@users.noreply.github.com>
2024-12-13 23:36:12 +0200

    fix: initialize premise list

--- ForwardChaining_Propositional/src/ai/ImplicationForm.java
+++ ForwardChaining_Propositional/src/ai/ImplicationForm.java
@@ -1,10 +1,11 @@
 package ai;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class ImplicationForm
 {
-	private List<String> premise;
+	private List<String> premise= new ArrayList<>();
 	private String conclusion;
 
 	public ImplicationForm(List<String> premise, String conclusionString)
