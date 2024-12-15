2835c64ec267a3c5ba0a7620a97fdc100f0740d8
BabisDros <150071850+BabisDros@users.noreply.github.com>
2024-12-15 19:14:15 +0200

    refactor: remove debugs and change User UI

--- ForwardChaining_FirstOrder/src/ai/FOL.java
+++ ForwardChaining_FirstOrder/src/ai/FOL.java
@@ -9,8 +9,7 @@ import java.util.Set;
 
 public class FOL
 {
-	
-	Standarize standarize=new Standarize();
+	Standarize standarize = new Standarize();
 
 	public Map<String, String> fc_Ask(Set<Implication> KB, Implication a)
 	{
@@ -18,44 +17,40 @@ public class FOL
 		{
 			// resetCounter
 			standarize.resetCounter();
+
 			Set<Implication> newSentences = new HashSet<>();
 
 			for (Implication currentImplication : KB)
 			{
 				standarize.newVars(currentImplication);
+
 				Map<String, String> substitutions = findImplicationMatch(currentImplication, KB);
 
 				// there is an available substitution for current implication
 				if (substitutions.size() > 0)
 				{
-					System.out.println("current impl: " + currentImplication + "subs:" + substitutions);
-
 					Predicate newFact = new Predicate(currentImplication.getConclusionPredicate());
 
 					// replace variables in conclusion
 					List<Term> conclusionTerms = newFact.getTerms();
 					for (Map.Entry<String, String> entry : substitutions.entrySet())
 					{
-//			            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
 						for (Term term : conclusionTerms)
 						{
-							
+
 							if (term.name.equals(entry.getKey()))
 							{
-								term.isVariable=false;
+								term.isVariable = false;
 								term.name = entry.getValue();
 							}
 						}
 					}
-					// print to check correct substitution
-					System.out.println(newFact);
 
 					Implication newSentence = new Implication(newFact);
 					if (!findPredicateMatch(newFact, KB) && !findPredicateMatch(newFact, newSentences))
-//					if (!KB.contains(newSentence) && !newSentences.contains(newSentence))
 					{
 						newSentences.add(newSentence);
-						
+
 						// if currentConclusion matches input from user
 						if (newFact.getName().equals(a.getConclusionPredicate().getName()))
 						{
@@ -68,7 +63,6 @@ public class FOL
 
 							for (int i = 0; i < conclusionTerms.size(); i++)
 							{
-
 								Term currentTerm = conclusionTerms.get(i);
 								Term otherTerm = inputTerms.get(i);
 
@@ -78,38 +72,17 @@ public class FOL
 									System.out.println("cant unifiy");
 									break;
 								}
-
-//									System.out.println(currentPredicate + " current: " + currentTerm + " other: " + otherTerm);
 							}
 
 							if (canUnify)
 							{
-//								System.out.println(implication + "=========  PRED: " + currentPredicate + " subs: "
-//										+ unifier.substitutions);
-								System.out.println("Subs: "+unifier.substitutions);
 								return unifier.substitutions;
 							}
 						}
 					}
-
 				}
-
-//			for(Implication implication:KB)
-//			{
-//				System.out.println(implication+" size:"+implication.getPremisePredicates().size());
-//			}
-
 			}
-			System.out.println("\nSTART current facts ");
-			for(Implication currentImplication : KB)
-			{
-				
-				if(currentImplication.isFact())
-					System.out.println(currentImplication);
-				
-			}
-			
-			System.out.println("END current facts \n");
+
 			if (newSentences.isEmpty())
 			{
 				return null;
@@ -117,7 +90,6 @@ public class FOL
 
 			KB.addAll(newSentences);
 		}
-
 	}
 
 	Map<String, String> findImplicationMatch(Implication implication, Set<Implication> KB)
@@ -129,7 +101,6 @@ public class FOL
 
 		for (Predicate currentPredicate : currentPremisePredicates)
 		{
-
 			boolean foundMatchForPredicate = false;
 
 			// Compare current predicate with facts in KB
@@ -148,7 +119,6 @@ public class FOL
 
 					for (int i = 0; i < currentPredicateTerms.size(); i++)
 					{
-
 						Term currentTerm = currentPredicateTerms.get(i);
 						Term otherTerm = otherImplicationTerms.get(i);
 
@@ -156,27 +126,19 @@ public class FOL
 						{
 							canUnify = false;
 							foundMatchForPredicate = false;
-							// return new HashMap<String, String>();
 							break;
 						}
-
-//							System.out.println(currentPredicate + " current: " + currentTerm + " other: " + otherTerm);
 					}
 
 					if (canUnify)
 					{
-//						System.out.println(implication + "=========  PRED: " + currentPredicate + " subs: "
-//								+ unifier.substitutions);
 						premiseTotalSubstitutions.putAll(unifier.substitutions);
 					}
-
 				}
 			}
 			if (!foundMatchForPredicate)
 			{
 				foundMatchForAllPremises = false;
-//				System.out.println("Predicate: " + currentPredicate + "of implication: " + implication
-//						+ " didnt find a matching fact");
 			}
 		}
 
@@ -193,7 +155,6 @@ public class FOL
 
 	boolean findPredicateMatch(Predicate predicate, Set<Implication> listToSearchIn)
 	{
-
 		boolean foundMatchForPredicate = false;
 
 		for (Implication otherImplication : listToSearchIn)
@@ -202,93 +163,98 @@ public class FOL
 					&& otherImplication.getConclusionPredicate().getName().equals(predicate.getName()))
 			{
 				foundMatchForPredicate = true;
+
 				// check if their terms can be unified
 				List<Term> otherImplicationTerms = otherImplication.getConclusionPredicate().getTerms();
 				List<Term> currentPredicateTerms = predicate.getTerms();
 
 				Unifier unifier = new Unifier();
-				boolean canUnify = true;
 
 				for (int i = 0; i < currentPredicateTerms.size(); i++)
 				{
-
 					Term currentTerm = currentPredicateTerms.get(i);
 					Term otherTerm = otherImplicationTerms.get(i);
 
 					if (!unifier.unify(currentTerm, otherTerm))
 					{
-						canUnify = false;
-						// return new HashMap<String, String>();
 						System.out.println("cant unify");
 						break;
 					}
-
-//						System.out.println(currentPredicate + " current: " + currentTerm + " other: " + otherTerm);
 				}
-
-				if (canUnify)
-				{
-//					System.out.println("=========  PRED: " + predicate + " subs: " + unifier.substitutions);
-				}
-
 			}
 		}
 		return foundMatchForPredicate;
 	}
 
-	
-
 	public static void main(String[] args)
 	{
-		FOLLoader loader = new FOLLoader();
+		// color option from :
+		// https://www.tutorialspoint.com/how-to-print-colored-text-in-java-console#:~:text=Step%2D1%3A%20Create%20ANSI%20escape,formatting%20to%20its%20original%20condition.
+		String RESET = "\u001B[0m";
+		String RED_TEXT = "\u001B[31m";
+		String GREEN_TEXT = "\u001B[32m";
+		
+		FOLLoader loader = new FOLLoader();	
 
-		loader.initialize("KB_FOL.txt");
-
-//		for(Implication implication:loader.KB)
-//		{
-//			System.out.println(implication+" size:"+implication.getPremisePredicates().size());
-//		}
 		FOL fol = new FOL();
 
+		String option, input, inputFileName;
 		try (Scanner keyboard = new Scanner(System.in))
 		{
 			while (true)
 			{
-				System.out.print("Εισάγετε τον τύπο προς απόδειξη: ");
+				printMenu();
+				System.out.print("Option: ");
+				option = keyboard.nextLine();
 
-				String input = keyboard.nextLine();
-
-				if (input.equals("0"))
+				if (option.equals("0"))
 				{
 					System.out.println("Exiting program...");
 					break;
-
 				}
 
-				// color option from :
-				// https://www.tutorialspoint.com/how-to-print-colored-text-in-java-console#:~:text=Step%2D1%3A%20Create%20ANSI%20escape,formatting%20to%20its%20original%20condition.
-				String RESET = "\u001B[0m";
-				String RED_TEXT = "\u001B[31m";
-				String GREEN_TEXT = "\u001B[32m";
-
-				Predicate inputPredicate = loader.convertInput(input);
-				Implication inputSentence = new Implication(inputPredicate);
-
-				System.out.println("INPUT PREDICATE"+ inputPredicate.getTerms());
-				Map<String, String> answer = fol.fc_Ask(loader.KB, inputSentence);
-				if (answer != null)
+				else if(option.equals("1"))
 				{
-					System.out.println(GREEN_TEXT + "True" + RESET+" answer: "+answer);
-				}
-				else
-				{
-					System.out.println(RED_TEXT + "False" + RESET);
-					System.out.println("Εισάγετε νέο τυπο ή 0 για έξοδο");
+					System.out.println("Please enter the file name to load (without .txt) or press enter to load default");
+				
+					inputFileName=keyboard.nextLine();
+					if(inputFileName.isEmpty())
+					{
+						loader.initialize("KB_FOL.txt");
+					}
+					else 
+					{
+						loader.initialize(inputFileName+".txt");
+					}
+					
+					System.out.println("Enter the sentence(accepted format:e.g Criminal(x) ) : ");
+					input = keyboard.nextLine();
+
+					Predicate inputPredicate = loader.convertInput(input);
+					Implication inputSentence = new Implication(inputPredicate);
+
+					Map<String, String> answer = fol.fc_Ask(loader.KB, inputSentence);
+								
+					if (answer != null)
+					{
+						System.out.println(GREEN_TEXT + "True" + RESET + " answer: " + answer);
+					}
+					else
+					{
+						System.out.println(RED_TEXT + "False" + RESET);
+					}
 				}
 			}
 
 			keyboard.close();
 		}
 		System.exit(0);
+	}
+
+	public static void printMenu()
+	{
+		System.out.println("\nWhat would you like to do?");
+		System.out.println("0. Exit");
+		System.out.println("1. Enter a sentence to prove");	
 	}
 }
--- ForwardChaining_FirstOrder/src/ai/FOLLoader.java
+++ ForwardChaining_FirstOrder/src/ai/FOLLoader.java
@@ -231,7 +231,6 @@ public class FOLLoader
 	    }
 	    return predicate;
 	}
-
 }
 
 
